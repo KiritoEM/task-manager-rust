5ad588c2de307c11293d5e9af6a2748e1a4936f5
KiritoEM <johankirito64@gmail.com>
2025-02-14 10:24:01 +0300

    chore: add gitignore

--- target/debug/deps/task_manager_rust-675abcf6d98890d3.d
+++
@@ -1,5 +0,0 @@
-/home/kirito/Desktop/kirito-dev/task-manager-rust/target/debug/deps/libtask_manager_rust-675abcf6d98890d3.rmeta: src/main.rs
-
-/home/kirito/Desktop/kirito-dev/task-manager-rust/target/debug/deps/task_manager_rust-675abcf6d98890d3.d: src/main.rs
-
-src/main.rs:
