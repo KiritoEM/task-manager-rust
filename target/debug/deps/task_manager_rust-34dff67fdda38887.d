15f7e3b68358944116c0df212a422320138b709d
KiritoEM <johankirito64@gmail.com>
2025-02-14 10:22:46 +0300

    first commit

---
+++ target/debug/deps/task_manager_rust-34dff67fdda38887.d
@@ -0,0 +1,5 @@
+/home/kirito/Desktop/kirito-dev/task-manager-rust/target/debug/deps/libtask_manager_rust-34dff67fdda38887.rmeta: src/main.rs
+
+/home/kirito/Desktop/kirito-dev/task-manager-rust/target/debug/deps/task_manager_rust-34dff67fdda38887.d: src/main.rs
+
+src/main.rs:
