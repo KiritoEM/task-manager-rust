9f03c2dc2a6cc57915c72dae2fd57d5ba064557c
KiritoEM <johankirito64@gmail.com>
2025-02-15 11:17:22 +0300

    feat: add json-parser and task-file-load

--- src/file.rs
+++ src/file.rs
@@ -1,13 +1,14 @@
 use std::fs::File;
 use std::io::{Read, Error};
-use crate::{parse_str_to_json, Task};
+use crate::parser::parse_str_to_json;
+use crate::task::Task;
 
 pub fn load_tasks_file(path: String) -> Result<Vec<Task>, Error>{
-    let mut file = File::open(path).expect("No such file, please verify the path");
+    let mut file = File::open(path).expect("No such file in path {path}, please verify the path");
     let mut contents = String::new();
 
-    file.read_to_string(&mut contents)?;
-    
+    file.read_to_string(&mut contents).expect("Unable to read file");
+
     let tasks: Vec<Task> = match parse_str_to_json(&contents) {
         Ok(tasks) => tasks,
         Err(e) => return Err(e),
